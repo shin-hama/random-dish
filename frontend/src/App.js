96419b0bc2b4ec81c057d71c2d2dca730c1ace4f
shin-hama <htysivista@gmail.com>
2021-03-23 23:09:41 +0900

    FEAT: Create first api fot test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import axios from 'axios'
 
 function App() {
-  return <div>test</div>
+  const [test, setTest] = React.useState({ test: '' })
+  const getTest = () => {
+    axios
+      .get('http://127.0.0.1:8000')
+      .then((response) => {
+        console.log(response)
+        setTest(response.data)
+      })
+      .catch(() => {
+        console.log('error')
+      })
+  }
+  React.useEffect(() => {
+    getTest()
+  }, [])
+  return <div>{test.test}</div>
 }
 
 export default App
