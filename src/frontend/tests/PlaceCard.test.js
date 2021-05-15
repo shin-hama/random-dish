616ad12482ca697e6c62e88dedfa2ffc50b04bc7
shin-hama <htysivista@gmail.com>
2021-05-15 21:38:48 +0900

    Remove event test

--- src/frontend/tests/PlaceCard.test.js
+++ src/frontend/tests/PlaceCard.test.js
@@ -21,65 +21,24 @@ test('Full data card', () => {
   expect(tree).toMatchSnapshot()
 })
 
-// test('Change to next photo to last', () => {
-//   const place = {
-//     name: 'all',
-//     photos: ['1', '2'],
-//     rating: 4.1,
-//     url: 'dummy/path.html',
-//   }
-//   const component = renderer.create(<PlaceCard place={place} id={1} />)
-//   let tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
+test('Change to next photo to last', () => {
+  const place = {
+    name: 'all',
+    photos: ['1', '2'],
+    rating: 4.1,
+    url: 'dummy/path.html',
+  }
+  const component = renderer.create(<PlaceCard place={place} id={1} />)
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 
-//   // Will change photo
-//   component.handleNext()
-//   tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
+  // Will change photo
+  component.handleNext()
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 
-//   // Will not change photo
-//   component.handleNext()
-//   tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
-// })
-
-// test('Will not change to previous photo from first', () => {
-//   const place = {
-//     name: 'all',
-//     photos: ['1', '2'],
-//     rating: 4.1,
-//     url: 'dummy/path.html',
-//   }
-//   const component = renderer.create(<PlaceCard place={place} id={1} />)
-//   let tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
-
-//   // Will not change photo
-//   renderer.act(() => {
-//     component.handleBack()
-//   })
-//   tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
-// })
-
-// test('Change to next and previous photo', () => {
-//   const place = {
-//     name: 'all',
-//     photos: ['1', '2'],
-//     rating: 4.1,
-//     url: 'dummy/path.html',
-//   }
-//   const component = renderer.create(<PlaceCard place={place} id={1} />)
-//   let tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
-
-//   // Will change to second photo
-//   component.handleNext()
-//   tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
-
-//   // Will change to first photo
-//   component.handleBack()
-//   tree = component.toJSON()
-//   expect(tree).toMatchSnapshot()
-// })
+  // Will not change photo
+  component.handleNext()
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
